22cd54f58b89a747f592778fe9f68207bbdee3c9
Grant Holley <gvholley@gmail.com>
2020-10-14 13:04:52 +0900

    Completed account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,15 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   def remember
     remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
