1ba724d85cf3017e5afc5a36c63873137a647ebf
Grant Holley <gvholley@gmail.com>
2020-10-21 18:06:59 +0900

    Changing picture upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,14 +1,14 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :photo, content_type: { in: %w[photo/jpeg photo/gif photo/png],
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
                                     message: "must be a valid image format" },
                     size:         { less_than: 5.megabytes,
                                     message: "should be less than 5MB" }
   def display_image
-    photo.variant(resize_to_limit: [500, 500])
+    image.variant(resize_to_limit: [500, 500])
   end
 end
